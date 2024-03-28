7bfa9afd8fb5b4d02b075f46d032358b878e3301
parbhatsingh5211 <parbhatsingh.sr@gmail.com>
2024-03-28 17:28:47 +0530

    recognize winndow size using react

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+  const [width, setWidth]=useState(window.innerWidth);
+  const [height, setHeight]=useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [width, height]);
+
+  return (
+    <div className="App">
+      <label>Window Size:  </label><br/>
+      <span>Width: {width} Height: {height}</span>
+    </div>
+  );
+}
+
+export default App;
